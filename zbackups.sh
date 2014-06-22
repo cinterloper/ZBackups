464ff0e5e87005e58f991ff0ae0e04ac35e36d3c
Grant <grant@iowntheinter.net>
2014-06-21 22:18:18 -0700

    umm more...

--- zbackups.sh
+++ zbackups.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 SRC="TI"
 DST="NUCBackup"
+INC="daily"
+
+
+if [ -e last.bklst ]
+ then
+	for i in $(cat next.bklst)
+	do
+		zfs send $i | pv | zfs recv $DST\@$(echo $i | cut -d '@' -f2)
+	done
 
 
 
 
 
 
-
-
-
-
-
-
-zfs list -t snapshot | grep $(date +"%Y-%m-%d") | grep $SRC | grep daily | cut -d ' ' -f 1 > next.bklst
+mv next.bklst last.bklst
+zfs list -t snapshot | grep $(date +"%Y-%m-%d") | grep $SRC | grep $INC | cut -d ' ' -f 1 > next.bklst
